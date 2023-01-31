7a88aede333bc5754817e5b7922a9e642fffce18
Thomas Lanquetin <tlanquetin@steamulo.com>
2023-01-31 17:26:24 +0100

    feat: add SF6 compatibility

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,13 +17,6 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('jjalvarezl_pd_fjs_viewer');
-        $rootNode = $treeBuilder->getRootNode();
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
-        return $treeBuilder;
+        return new TreeBuilder('jjalvarezl_pd_fjs_viewer');
     }
 }
